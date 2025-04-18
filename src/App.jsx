2c85eeb14974001dfbf1f2a4e878c559b7e2545a
Jayce <jayxalvarado@gmail.com>
2025-04-18 08:06:19 -0700

    feat: update index.html content and refactor App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div>
-      "Experience the convenience of delicious meals delivered straight to your
-      doorstep. Our food delivery app connects you with your favorite local
-      restaurants, offering a wide variety of cuisines to satisfy every craving.
-      Fast, reliable, and easy to use—your next meal is just a tap away!"
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
